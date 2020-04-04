4f522d7009b849c4088c8f88edd5e21ad8930fea
Phillip Henry <PhillHenry@gmail.com>
2020-04-04 10:45:03 +0100

    Increased FS2 version. Documented some results.

--- streamperf/src/main/scala/uk/co/odinconsultants/fp/performance/count/JMH_FS2_Count.scala
+++ streamperf/src/main/scala/uk/co/odinconsultants/fp/performance/count/JMH_FS2_Count.scala
@@ -11,6 +11,14 @@ import uk.co.odinconsultants.fp.performance.count.Data._
 /**
  * Run with something like:
  *  sbt jmh:run Bench -prof jmh.extras.JFR:help
+ * FS2 2.0.1
+[info]   19.766 ±(99.9%) 0.176 ops/s [Average]
+[info]   (min, avg, max) = (19.152, 19.766, 20.058), stdev = 0.235
+[info]   CI (99.9%): [19.590, 19.942] (assumes normal distribution)
+ * FS2 2.3.0
+[info]   19.697 ±(99.9%) 0.510 ops/s [Average]
+[info]   (min, avg, max) = (17.061, 19.697, 20.164), stdev = 0.681
+[info]   CI (99.9%): [19.187, 20.206] (assumes normal distribution)
  */
 class JMH_FS2_Count {
 
